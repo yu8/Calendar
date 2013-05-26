b1cffe20912f518efc5e7bed7147668359905fc0
yuya-shigeta <s.tyaro@gmail.com>
2013-05-26 12:01:31 +0900

    リファクタリング

--- calendar.rb
+++ calendar.rb
@@ -4,7 +4,7 @@ class Calender
   
   def initialize(year, month)
     @month = Date.new(year, month).strftime("%b")
-    @year = Date.new(year, month).year
+    @year = year
     @week = 'Su Mo Tu We Th Fr St'
     @month_end = Date.new(year, month, -1).day
     @month_start = Date.new(year, month, 1)
@@ -12,15 +12,15 @@ class Calender
   end
 
   def render
-    month_line = "#{@month}\s#{@year}" 
-    month_line_blank = (21 - month_line.size) / 2
+    title = "#{@month}\s#{@year}" 
+    title_blank = (21 - title.size) / 2
     week_line = "#{@week}"
     if @month_start.cwday == 7
       blank = 0
     else
       blank = @month_start.cwday * 3
     end    
-    @output = "\s" * month_line_blank + "#{month_line}\n#{week_line}\n" + "\s" * blank
+    @output = "\s" * title_blank + "#{title}\n#{week_line}\n" + "\s" * blank
     @days.each do |day|
       ret =  @month_start.cwday + (day - 1)
       if ret%7 == 6
@@ -29,14 +29,13 @@ class Calender
         @output += "#{day.to_s.rjust(2)}\s"
       end 
     end
-    puts @output
-    
+    @output
   end
 
 end
 
 (1..12).each do |month|
   calendar = Calender.new(2013, month)
-  calendar.render
+  puts calendar.render
   puts "----------------------------\n\n"
 end

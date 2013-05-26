d456ac54788194f1115f08b910f82e0865a4dd3f
yuya-shigeta <s.tyaro@gmail.com>
2013-05-26 20:10:42 +0900

    スペル間違い、変数のスコープを修正

--- calendar.rb
+++ calendar.rb
@@ -1,14 +1,13 @@
 require 'date'
 
-class Calender
+class Calendar
   
   def initialize(year, month)
     @month = Date.new(year, month).strftime("%b")
     @year = year
     @week = 'Su Mo Tu We Th Fr St'
-    @month_end = Date.new(year, month, -1).day
     @month_start = Date.new(year, month, 1)
-    @days = Array(1..@month_end)
+    @days = Array(1..Date.new(year, month, -1).day)
   end
 
   def render
@@ -20,22 +19,22 @@ class Calender
     else
       blank = @month_start.cwday * 3
     end    
-    @output = "\s" * title_blank + "#{title}\n#{week_line}\n" + "\s" * blank
+    output = "\s" * title_blank + "#{title}\n#{week_line}\n" + "\s" * blank
     @days.each do |day|
       ret =  @month_start.cwday + (day - 1)
       if ret%7 == 6
-        @output += "#{day.to_s.rjust(2)}\n"
+        output += "#{day.to_s.rjust(2)}\n"
       else
-        @output += "#{day.to_s.rjust(2)}\s"
+        output += "#{day.to_s.rjust(2)}\s"
       end 
     end
-    @output
+    output
   end
 
 end
 
 (1..12).each do |month|
-  calendar = Calender.new(2013, month)
+  calendar = Calendar.new(2013, month)
   puts calendar.render
   puts "----------------------------\n\n"
 end
